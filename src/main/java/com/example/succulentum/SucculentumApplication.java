06937b26cbc3b15aa10294848e5d6846b6eb0ba1
DmitryMustk <dmutrymust@gmail.com>
2024-07-04 14:14:54 +0700

    basic spring app

---
+++ src/main/java/com/example/succulentum/SucculentumApplication.java
@@ -0,0 +1,13 @@
+package com.example.succulentum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SucculentumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SucculentumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/succulentum/SucculentumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.succulentum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SucculentumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
