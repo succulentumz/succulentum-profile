b4ccf644d16a4c89da8f7ab0bff20b2add93121a
DmitryMustk <dmutrymust@gmail.com>
2024-11-08 07:05:21 +0700

    remove email verification for now

--- run_key_cloak.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --name suc-keycloak -p 8082:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin -v ./config/keycloak/import:/opt/keycloak/data/import quay.io/keycloak/keycloak:23.0.4 start-dev --import-realm
